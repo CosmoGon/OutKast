fadedf15783bdc675a39104ec446d6b9c2d036bc
Ghinea Cosmin <cosmin.ghinea47@gmail.com>
2024-11-27 13:27:51 +0200

    Initial Commit

---
+++ OutKast/OutKastApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutKastApp.swift
+//  OutKast
+//
+//  Created by Cosmin Ghinea on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct OutKastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
